8f8badb8cdc8b92867e1f49fdf357ac1b6dcc0af
Folklore Labs <git@folklorelabs.io>
2022-02-12 14:26:23 -0800

    better styling

--- src/components/PageWalletGen.jsx
+++ src/components/PageWalletGen.jsx
@@ -1,59 +1,64 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import Blockies from 'react-blockies';
 import { ethers } from 'ethers';
+import classnames from 'classnames';
 import AnchorAddress from './AnchorAddress';
 
 import './PageWalletGen.css';
-import { apiGetAccountNonce } from '../service/ethApi';
+// import { apiGetAccountNonce } from '../service/ethApi';
+
+const NEW_ROW_COUNT = 8;
+const MAX_WALLETS_VISIBLE = 32;
 
 function PageWalletGen() {
   const [randomWallets, setRandomWallets] = useState([]);
   const initFavs = JSON.parse(localStorage.getItem('favoriteWallets') || '[]');
   const [favoriteWallets, setFavoriteWallets] = useState(initFavs);
-  const [usedWallets, setUsedWallets] = useState({});
-  const [autoGen, setAutoGen] = useState(false);
+  const [usedWallets] = useState({});
+  const [autoGen] = useState(false);
 
   const genRow = useCallback(() => {
-    const newWallets = [];
-    for (let i = 0; i < 6; i += 1) {
+    const newRandomWallets = [];
+    for (let i = 0; i < NEW_ROW_COUNT; i += 1) {
       const wallet = ethers.Wallet.createRandom();
-      newWallets.push([wallet.address, wallet.mnemonic.phrase]);
+      newRandomWallets.push([wallet.address, wallet.mnemonic.phrase]);
     }
+    const oldWalletsTargetLength = MAX_WALLETS_VISIBLE - NEW_ROW_COUNT;
     setRandomWallets([
-      ...newWallets,
-      ...randomWallets,
+      ...randomWallets.slice(-oldWalletsTargetLength),
+      ...newRandomWallets,
     ]);
   }, [randomWallets]);
 
-  const updateUsed = useCallback(async () => {
-    const promises = [];
-    const addresses = [];
-    for (let i = 0; i < randomWallets.length; i += 1) {
-      const wallet = randomWallets[i];
-      const walletAddress = wallet[0];
-      if (typeof usedWallets[walletAddress] !== 'boolean') {
-        promises.push(apiGetAccountNonce(walletAddress, 1));
-        addresses.push(walletAddress);
-      }
-    }
-    for (let i = 0; i < favoriteWallets.length; i += 1) {
-      const wallet = favoriteWallets[i];
-      const walletAddress = wallet[0];
-      if (typeof usedWallets[walletAddress] !== 'boolean') {
-        promises.push(apiGetAccountNonce(walletAddress, 1));
-        addresses.push(walletAddress);
-      }
-    }
-    if (!promises.length) return;
-    const results = await Promise.all(promises);
-    const used = {
-      ...usedWallets,
-    };
-    for (let i = 0; i < results.length; i += 1) {
-      used[addresses[i]] = !!results[i];
-    }
-    setUsedWallets(used);
-  }, [randomWallets, favoriteWallets, usedWallets]);
+  // const updateUsed = useCallback(async () => {
+  //   const promises = [];
+  //   const addresses = [];
+  //   for (let i = 0; i < randomWallets.length; i += 1) {
+  //     const wallet = randomWallets[i];
+  //     const walletAddress = wallet[0];
+  //     if (typeof usedWallets[walletAddress] !== 'boolean') {
+  //       promises.push(apiGetAccountNonce(walletAddress, 1));
+  //       addresses.push(walletAddress);
+  //     }
+  //   }
+  //   for (let i = 0; i < favoriteWallets.length; i += 1) {
+  //     const wallet = favoriteWallets[i];
+  //     const walletAddress = wallet[0];
+  //     if (typeof usedWallets[walletAddress] !== 'boolean') {
+  //       promises.push(apiGetAccountNonce(walletAddress, 1));
+  //       addresses.push(walletAddress);
+  //     }
+  //   }
+  //   if (!promises.length) return;
+  //   const results = await Promise.all(promises);
+  //   const used = {
+  //     ...usedWallets,
+  //   };
+  //   for (let i = 0; i < results.length; i += 1) {
+  //     used[addresses[i]] = !!results[i];
+  //   }
+  //   setUsedWallets(used);
+  // }, [randomWallets, favoriteWallets, usedWallets]);
 
   useEffect(() => {
     if (!autoGen) return null;
@@ -72,43 +77,55 @@ function PageWalletGen() {
   return (
     <div className="PageWalletGen RainbowBG">
       <div className="PageWalletGen-inner">
-        <button
-          type="button"
-          onClick={() => genRow()}
-        >
-          gen row
-        </button>
-        <button
-          type="button"
-          onClick={() => setAutoGen(!autoGen)}
-        >
-          auto gen:
-          {' '}
-          {autoGen ? 'off' : 'on'}
-        </button>
-        <button
-          type="button"
-          onClick={async () => {
-            await updateUsed();
-          }}
-        >
-          check used
-        </button>
-        <div
-          style={{
-            display: 'flex', flexDirection: 'row', flexWrap: 'wrap', width: '100%', maxWidth: '708px',
-          }}
-        >
+        <div className="PageWalletGen-section">
+          <h1 className="PageWalletGen-headline">Wallet Generator</h1>
+          <span>
+            Click the button to start generating rows of potential wallets.
+            Click on wallets you like to save them below.
+          </span>
+        </div>
+        <div className="PageWalletGen-section">
+          {!autoGen ? (
+            <button
+              className="Button"
+              type="button"
+              onClick={() => genRow()}
+            >
+              Generate
+              {' '}
+              {NEW_ROW_COUNT}
+              {' '}
+              blockies
+            </button>
+          ) : ''}
+        </div>
+        {/* <div className="PageWalletGen-section">
+          <button
+            className={classnames(
+              'Button',
+              {
+                'Button--loading': autoGen,
+              },
+            )}
+            type="button"
+            onClick={() => setAutoGen(!autoGen)}
+          >
+            {autoGen ? 'Stop generating continously' : 'Generate continuously'}
+          </button>
+        </div> */}
+        <div className="PageWalletGen-section RandomWalletList">
           {randomWallets.map((wallet) => (
             <button
+              className={classnames(
+                'RandomWallet',
+                {
+                  'RandomWallet--used': usedWallets[wallet[0]],
+                  'RandomWallet--favorite': favoriteWallets.find((fw) => fw[0] === wallet[0]),
+                },
+              )}
               key={wallet[0]}
               id={wallet[0]}
               type="button"
-              style={{
-                padding: '20px',
-                borderWidth: 0,
-                background: usedWallets[wallet[0]] ? 'red' : 'transparent',
-              }}
               onClick={() => {
                 const newFavs = [
                   ...favoriteWallets,
@@ -126,50 +143,78 @@ function PageWalletGen() {
             </button>
           ))}
         </div>
-        <div>
-          <h1>SAVED</h1>
-          {favoriteWallets.map((wallet) => (
-            <div
-              key={wallet[0]}
-              style={{
-                background: usedWallets[wallet[0]] ? 'red' : 'transparent',
+        {favoriteWallets.length ? (
+          <div>
+            <h1>SAVED WALLETS</h1>
+            {/* <button
+              type="button"
+              onClick={async () => {
+                await updateUsed();
               }}
             >
-              <Blockies
-                seed={wallet[0].toLowerCase()}
-                scale={9}
-              />
-              <AnchorAddress address={wallet[0]} />
-              <input
-                id={`mnemonic-${wallet[0]}`}
-                defaultValue={wallet[1]}
-                type="text"
-                disabled
-              />
-              <button
-                type="button"
-                onClick={() => {
-                  const inputMnemonic = document.getElementById(`mnemonic-${wallet[0]}`);
-                  inputMnemonic.select();
-                  inputMnemonic.setSelectionRange(0, 99999);
-                  navigator.clipboard.writeText(inputMnemonic.value);
+              check if any saved wallets have been used
+            </button> */}
+            {favoriteWallets.map((wallet) => (
+              <div
+                className="FavoriteWallet"
+                key={wallet[0]}
+                style={{
+                  background: usedWallets[wallet[0]] ? 'red' : 'transparent',
+                  display: 'flex',
                 }}
               >
-                copy
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  const newFavs = favoriteWallets.filter((fav) => fav[0] !== wallet[0]);
-                  localStorage.setItem('favoriteWallets', JSON.stringify(newFavs));
-                  setFavoriteWallets(newFavs);
-                }}
-              >
-                x
-              </button>
-            </div>
-          ))}
-        </div>
+                <Blockies
+                  seed={wallet[0].toLowerCase()}
+                  scale={9}
+                />
+                <div style={{ textAlign: 'left', marginLeft: '5px' }}>
+                  <div>
+                    <AnchorAddress address={wallet[0]} />
+                  </div>
+                  <div>
+                    <input
+                      id={`mnemonic-${wallet[0]}`}
+                      defaultValue={wallet[1]}
+                      type="text"
+                      disabled
+                    />
+                    <button
+                      className="AnchorLink"
+                      type="button"
+                      onClick={() => {
+                        const inputMnemonic = document.getElementById(`mnemonic-${wallet[0]}`);
+                        inputMnemonic.select();
+                        inputMnemonic.setSelectionRange(0, 99999);
+                        navigator.clipboard.writeText(inputMnemonic.value);
+                      }}
+                    >
+                      copy secret phrase
+                    </button>
+                  </div>
+                  <div>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        const newFavs = favoriteWallets.filter((fav) => fav[0] !== wallet[0]);
+                        localStorage.setItem('favoriteWallets', JSON.stringify(newFavs));
+                        setFavoriteWallets(newFavs);
+                      }}
+                      style={{
+                        backgroundColor: 'transparent',
+                        border: 'none',
+                        color: 'red',
+                        cursor: 'pointer',
+                        padding: 0,
+                      }}
+                    >
+                      unfavorite
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : ''}
       </div>
     </div>
   );
