0f8b1ba126c1952e47d0176d8c10cd16b45f74fd
Dustin Boersma <dustin.boersma@gmail.com>
2022-02-12 16:29:53 -0800

    new styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
 import './App.css';
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import AppHeader from './AppHeader';
+// import AppHeader from './AppHeader';
 import AppFooter from './AppFooter';
 import PageWalletGen from './PageWalletGen';
 
 function App() {
   return (
     <Router>
-      <div className="App">
+      <div className="App RainbowBG">
         <div className="App-inner">
-          <AppHeader className="App-header" />
+          {/* <AppHeader className="App-header" /> */}
           <main className="App-main">
             <Routes>
               <Route
